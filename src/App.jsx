c31a2d84e5be6045cc90f383bd31781a973bf88c
Bomuratov <antikko99@gmail.com>
2025-01-15 22:30:48 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import NotFound from './apps/client/components/NotFound'
+import Login from './apps/admin/pages/Login'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import AdminRoutes from './routes/AdminRoutes'
+import VendorRoutes from './routes/VendorRoutes'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Login />} />
+
+        {/*client and admin routes  */}
+        <Route path="/admin/:res/*" element={<AdminRoutes />} />
+        <Route path="vendor/:res/:table?/*" element={<VendorRoutes />} />
+      </Routes>
+
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
